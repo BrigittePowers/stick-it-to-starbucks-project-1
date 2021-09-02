e1c67d2fb5fe65b2fe2c7476f882f40c52a2c73d
Brigitte Powers PC <reyhn701@gmail.com>
2021-09-01 19:17:15 -0500

    Created psuedocoded index.js

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+
+// user loads the page
+
+// input fields are preset with last saved zip and distance (if applicable)
+
+// zip code required
+
+// if user doesn't enter zip code but presses SEARCH btn, highlight zip code input red
+
+// default distance x miles (distance optional) -- or dropdown box
+
+// if there is no recent search, input fields display placeholder text
+
+// user can see banner with our logo 
+
+// user can see a search form asking for their zip code and distance willing to travel [2 inputs]
+
+// user can see a SEARCH button to confirm inputs and perform the search
+
+// when user types in input fields... 
+
+// locally store their zip and distance preference 
+
+// user presses SEARCH 
+
+// clear dynamic elements 
+
+// create elements for...
+
+// ...map, 
+
+// ...and results page
+
+// page shows top three results for local coffee shops within user prefered distance (top 3 based on distance)
+
+// geolocate the user with Google API
+
+// Input user search in SERP API
+
+// pull and display data from top three searches
+
+// when user clicks result
+
+// user can pin or star search results that will be locally stored for later viewing
+
+// user will be shown embedded map direction information / link to that coffee shop's website
+
+// back button to return to results
+
+// below the 3 viewable results will be a link to the google search on google.com to view full results
