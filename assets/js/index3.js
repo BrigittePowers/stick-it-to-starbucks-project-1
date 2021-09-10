503d93e6629dd7b48bda6c516e630b4b917e74ab
Deni Mazzei <dconkell@gmail.com>
2021-09-10 15:51:32 -0500

    added Joey's JS to index3.js to test

--- assets/js/index3.js
+++ assets/js/index3.js
@@ -17,3 +17,16 @@ console.log(is_usZipCode("78254"));
 
 console.log(is_usZipCode("1234"));
 
+var search = document.getElementsByTagName('search')[0],
+  searchData = {
+    searchData: 'Results',
+    searchDescription: 'Result Direction'
+  },
+  localData;
+
+  localStorage.setItem('searchData', JSON.stringify(searchData));
+
+  localData = JSON.parse(localStorage.getItem('searchData'));
+
+  console.log(localData);
+  console.log(localStorage.getItem('searchData'));
