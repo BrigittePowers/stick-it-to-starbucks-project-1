c0a7737997ca76a7b6eb1f7895ebea8bde0edb1a
Brigitte Powers PC <reyhn701@gmail.com>
2021-09-11 13:15:17 -0500

    Added dynamic elements and franchise filter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 // HTML handles
 var searchBtn = document.querySelector(".searchBtn");
 var select = document.getElementById("mileage");
-var destination = document.querySelector(".destination");
+var resultsDisplay = document.querySelector(".results");
 
 // API handles
 var googleAPIKey = "AIzaSyCtojOrOmevqFcBO6zPY4W3rdfluhyMWpk";
@@ -9,25 +9,77 @@ var searchRadius = 0;
 var map;
 var service;
 var infowindow;
+var filterArr = [];
 
 
 //list of franchises to remove from search
-var corporateCoffee = ["Starbucks", "Costa Coffee", "Tim Hortons", "Dunkin Donuts", "Dunkin", "Peet's Coffee", "Tully's", "McDonald's", "McCafe", "Tealicious Cafe", "Shipley Do-Nuts", "Panera Bread"];
+var franchiseFilter = ["Starbucks", "Costa Coffee", "Tim Hortons", "Dunkin Donuts", "Dunkin", "Peet's Coffee", "Tully's", "McDonald's", "McCafe", "Tealicious Cafe", "Shipley Do-Nuts", "Panera Bread", "Barnes & Noble"];
 
-// Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=29.5632896%2C-98.6710016&radius=8046.72&type=cafe&key=AIzaSyCtojOrOmevqFcBO6zPY4W3rdfluhyMWpk. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing).
+//user clicks SEARCH, search parameters saved to variables
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
 
-// geolocation marker handles
-var locations = [
-    ['You are Here', 0, 0],
-    ['Philz Coffee<br>\
-    525 Santa Monica Blvd, Santa Monica, CA 90401<br>\
-    <a href="https://goo.gl/maps/PY1abQhuW9C2">Get Directions</a>', 
-    34.017951, 
-    -118.493567],
-    ['Result 2', 33.849182, -118.388405],
-    ['Result 3', 33.628342, -117.927933],
-];
+    //read distance dropdown box and convert to meters
+    var miles = select.value;
+    searchRadius = getMeters(miles);
 
+    //initMap should be last on the event listener after data is plugged into variables
+    initMap();
+});
+
+
+// == FUNCTIONS (a-z)==
+
+// returns results data
+function callback(results, status) {
+    // places service status is working
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // for the length of the results array
+        for (var i = 0; i < results.length; i++) {
+            // so long as data's name doesn't match our franchise filter
+            if (franchiseFilter.indexOf(results[i].name) === -1) {
+                // concat onto a new filtered array
+                filterArr.push(results[i]);
+            }
+        }
+
+        // display 3 closest results and marker
+        for (var i = 0; i < 3; i++) {
+            createMarker(filterArr[i], map);
+
+            // create elements
+            var rName = document.createElement('h4');
+            var rList = document.createElement('ul');
+            var rVicinity = document.createElement('li');
+            var rDirections = document.createElement('li');
+
+            // set element data
+            rName.innerHTML = filterArr[i].name;
+            rVicinity.innerHTML = filterArr[i].vicinity;
+            rDirections.innerHTML = "<a href='" + 'https://www.google.com/maps?saddr=My+Location&daddr=' + filterArr[i].geometry.location.lat() + '%2C' + filterArr[i].geometry.location.lng() + "'>Get Directions</a>";
+            //rLink.href = 'https://www.google.com/maps?saddr=My+Location&daddr=' + filterArr[i].geometry.location.lat() + '%2C' + filterArr[i].geometry.location.lng();
+
+            // append results to list
+            rList.appendChild(rVicinity);
+            rList.appendChild(rDirections);
+
+            // show elements on results div
+            resultsDisplay.appendChild(rName);
+            resultsDisplay.appendChild(rList);
+        }
+
+
+        // offer the remaining results with VIEW ALL link
+        
+        //console log data
+        console.log(results);
+
+    } else // places service status not working
+        console.log("API Server is currently unreachable");
+}
+
+
+// creates waypoints
 function createMarker(place, map) {
     if (!place.geometry || !place.geometry.location) return;
 
@@ -46,11 +98,12 @@ function createMarker(place, map) {
     });
 }
 
+// converts miles to meters
 function getMeters(i) {
     return i*1609.344;
 }
 
-//create the map using var LOCATIONS
+// create the map & results page
 async function initMap() {
 
     infowindow = new google.maps.InfoWindow();
@@ -67,88 +120,30 @@ async function initMap() {
                 lng: position.coords.longitude,
                 };
                 map.setCenter(pos);
-
-                //locations[0][1] = pos.lat;
-                //locations[0][2] = pos.lng;
-
-                //var cafeData = findCafe();
                 
+                var center = new google.maps.LatLng(pos.lat, pos.lng)
+
                 var request = {
-                    query: "coffee",
-                    fields: ["name", "formatted_address", "geometry", "icon", "place_id"],
-                    locationBias: {radius: searchRadius, center: {lat: pos.lat, lng: pos.lng}}
+                    location: center,
+                    radius: searchRadius,
+                    type: ['cafe'],
+                    keyword: "coffee",
                 };
-            
-                service = new google.maps.places.PlacesService(map)
 
-                service.findPlaceFromQuery(request, function(results, status) {
-                    if (status === google.maps.places.PlacesServiceStatus.OK) {
-                        for (var i = 0; i < results.length; i++) {
-                            createMarker(results[i], map);
-
-                            //Result name 
-                            var resultName = document.createElement("h4");
-                            resultName.innerHTML = results[i].name;
-
-                            // Result info
-                            var resultInfo = document.createElement("ul");
-                            var resultAddress = document.createElement("li");
-                            resultAddress.innerHTML = results[i].formatted_address;
-
-                            // Append
-                            resultInfo.appendChild(resultAddress);
-                            destination.appendChild(resultName);
-                            destination.appendChild(resultInfo);
-                        }
-                    }
-                });
-                
+                service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, callback);
 
             },
             () => {
-                handleLocationError(true, infoWindow, map.getCenter());
+                handleLocationError(true, infowindow, map.getCenter());
             }
         );
-        
-        /*
-        
-        var marker, count;
-        
-        for (count = 0; count < locations.length; count++) {
-            marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[count][1], locations[count][2]),
-            map: map,
-            title: locations[count][0]
-            });
-            
-            google.maps.event.addListener(marker, 'click', (function (marker, count) {
-                return function () {
-                infowindow.setContent(locations[count][0]);
-                infowindow.open(map, marker);
-                }
-            })(marker, count));
-        }
-        */
 
     } else {
         // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        handleLocationError(false, infowindow, map.getCenter());
     }
 }
-
-// user loads the page
-
-//user clicks SEARCH, search parameters saved to variables
-searchBtn.addEventListener("click", function(event){
-    event.preventDefault();
-
-    //read distance dropdown box and convert to meters
-    var miles = select.value;
-    searchRadius = getMeters(miles);
-
-    //initMap should be last on the event listener after data is plugged into variables
-    initMap();
-});
 
 // Local store data to confirm RETURNING USER from now on
     // if user is FIRST TIME USER
@@ -159,34 +154,5 @@ searchBtn.addEventListener("click", function(event){
         // modal will not display for RETURNING USER
 
 // user sees a button/link to go to favorite list
-
-// dynamic display landing page
-    // form
-        // text input zip code
-        // dropdown distance options
-        // submit button performs SEARCH
-            // check that zip code was input
-                // if zip code is input, record input + distance selection
-                // if zip code is undefined, highlight input red and inform user REQUIRED
-            // inputs are locally stored for RETURNING USER
-    // form answers pull from local storage if RETURNING USER to show last search options selected
-    // form answers default to placeholder-zip (Zip code needed...) and middle distance dropdown option 
-
-// Once SEARCH is performed
-    // remove dynamic landing page elements
-    // create dynamic search page elements
-        // show map showing pins for top 3 search results (closest by distance)
-        // show list of 3 results
             
-
-// when user clicks result
-    // clicking an icon (fontawesome? star) allows the user to save the result to a favorites list (user feedback: icon changes color)
-    // user will be shown embedded map direction information 
-    // link to that coffee shop's data
-        // name
-        // website or menu link
-        // address
-        // hours
-        // price (?)
-
 // back button to return to results
