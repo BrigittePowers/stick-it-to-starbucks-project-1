43a41ca7d5da2ae152b71933fa5ea80ec53e1c3d
Brigitte Powers PC <reyhn701@gmail.com>
2021-09-13 13:41:17 -0500

    Merging Features together

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,17 @@ var resultsDisplay = document.querySelector(".results");
 var resultsTitle = document.querySelector(".results-title");
 var resultsWrap = document.getElementById("destination-section");
 
+// local storage handles
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h2 = document.querySelector('h2');
+const personalGreeting = document.querySelector('.personal-greeting');
+
 // API handles
 var googleAPIKey = "AIzaSyCtojOrOmevqFcBO6zPY4W3rdfluhyMWpk";
 var searchRadius = 0;
@@ -68,17 +79,11 @@ function callback(results, status) {
             resultsDisplay.appendChild(rName);
             resultsDisplay.appendChild(rList);
         }
-
-
-        // offer the remaining results with VIEW ALL link
-        var rFullList = document.createElement('button');
-        rFullList.innerHTML = "View All Results";
-        resultsDisplay.appendChild(rFullList);
         
         //console log data
         console.log(results);
 
-        //user clicks SEARCH, search parameters saved to variables
+        //user clicks VIEW ALL, search parameters saved to variables
         rFullList.addEventListener("click", function(event){
             event.preventDefault();
 
@@ -161,14 +166,49 @@ async function initMap() {
     }
 }
 
-// Local store data to confirm RETURNING USER from now on
-    // if user is FIRST TIME USER
-        // display modal explaining purpose
-        // mark user as RETURNING USER once modal is closed
+// LOCAL STORAGE
+// Stop the form from submitting when a button is pressed
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
 
-    // else user is RETURNING USER
-        // modal will not display for RETURNING USER
+  // run function when the 'Say hello' button is clicked
+submitBtn.addEventListener('click', function() {
+    // store the entered name in web storage
+    localStorage.setItem('name', nameInput.value);
+    // run nameDisplayCheck() to sort out displaying the
+    // personalized greetings and updating the form display
+    nameDisplayCheck();
+});
 
-// user sees a button/link to go to favorite list
-            
-// back button to return to results
+  // run function when the 'Forget' button is clicked
+forgetBtn.addEventListener('click', function() {
+    // Remove the stored name from web storage
+    localStorage.removeItem('name');
+    // run nameDisplayCheck() to sort out displaying the
+    // generic greeting again and updating the form display
+    nameDisplayCheck();
+});
+
+// define the nameDisplayCheck() function
+function nameDisplayCheck() {
+    // check whether the 'name' data item is stored in web Storage
+    if(localStorage.getItem('name')) {
+      // If it is, display personalized greeting
+        let name = localStorage.getItem('name');
+        h2.textContent = 'Hi, ' + name;
+        personalGreeting.textContent = 'Let Us Find A Local Coffee Shop For You!';
+      // hide the 'remember' part of the form and show the 'forget' part
+        forgetDiv.style.display = 'block';
+        rememberDiv.style.display = 'none';
+    } else {
+    // if not, display generic greeting
+        h2.textContent = 'Hi there! ';
+        personalGreeting.textContent = 'Let Us Find A Local Coffee Shop For You';
+    // hide the 'forget' part of the form and show the 'remember' part
+        forgetDiv.style.display = 'none';
+        rememberDiv.style.display = 'block';
+    }
+}
+
+document.body.onload = nameDisplayCheck;
